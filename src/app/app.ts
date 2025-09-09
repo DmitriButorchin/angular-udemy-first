c54814420dd5ed598bb93470b96d0e18b31f270f
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2025-09-09 13:08:54 -0400

    add header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Header } from "./header";
+import { Header } from './header/header';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [Header]
+  imports: [Header, User],
 })
 export class App {}
