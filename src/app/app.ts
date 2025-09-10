2fa9311c1f155d29435d5f3739170a81bfee2724
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2025-09-10 10:16:41 -0400

    add conditionals

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,7 +12,7 @@ import { User } from './user/user';
 })
 export class App {
   users = DUMMY_USERS;
-  selectedUserId = 'u1';
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
