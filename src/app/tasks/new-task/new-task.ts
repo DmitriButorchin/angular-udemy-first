beb74f28bd73d32530c3e282369bd06e3ebf3274
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2025-09-10 17:03:38 -0400

    add two-way binding for the new task component

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,13 +1,17 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css',
 })
 export class NewTask {
   cancel = output<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel() {
     this.cancel.emit();
